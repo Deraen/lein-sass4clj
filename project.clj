8ee7c0cb29183f9ba8a229d0a154119a289951b5
Juho Teperi <juho.teperi@iki.fi>
2015-01-27 19:58:07 +0200

    Initial version

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject lein-less4j "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[deraen/less4clj "0.1.0-SNAPSHOT"]]
+  :eval-in-leiningen true)
---
+++ src/leiningen/less4j.clj
@@ -0,0 +1,62 @@
+(ns leiningen.less4j
+  (:require
+    [leiningen.help]
+    [leiningen.core.eval :as leval]
+    [leiningen.core.project :as project]
+    [clojure.java.io :as io]))
+
+(defn main-file? [file]
+  (.endsWith (.getName file) ".main.less"))
+
+(defn find-main-files [source-paths]
+  (->> source-paths
+       (map (fn [source-path]
+              (let [file (io/file source-path)]
+                (->> (file-seq file)
+                     (filter main-file?)
+                     (map (fn [x] [(.getPath x) (.toString (.relativize (.toURI file) (.toURI x)))]))))))
+       (apply concat)))
+
+(def less4j-profile {:dependencies '[[deraen/less4clj "0.1.0-SNAPSHOT"]]})
+
+(defn- run-compiler
+  "Run the lesscss compiler."
+  [project
+   {:keys [source-paths target-path source-map compression]
+    :or {source-map false
+         compression false}}
+   watch?]
+  (let [project' (project/merge-profiles project [less4j-profile])]
+    (doseq [[path relative-path] (find-main-files source-paths)]
+      (leval/eval-in-project
+        project'
+        `(less4clj.core/less-compile
+           ~path
+           ~(.getPath (io/file target-path))
+           ~relative-path
+           {:source-map ~source-map
+            :compression ~compression})
+        '(require 'less4clj.core)))))
+
+(defn- once
+  "Compile less files once."
+  [project config]
+  (run-compiler project config false))
+
+(defn- auto
+  "Compile less files, then watch for changes and recompile until interrupted."
+  [project config]
+  (run-compiler project config true))
+
+(defn less4j
+  "Run the {less} css compiler plugin."
+  {:help-arglists '([once auto])
+   :subtasks      [#'once #'auto]}
+  ([project]
+   (println (leiningen.help/help-for "less4j"))
+   (leiningen.core.main/abort))
+  ([project subtask & args]
+   (let [config (:less project)]
+     (case subtask
+       "once" (apply once project config args)
+       "auto" (apply auto project config args)))))
