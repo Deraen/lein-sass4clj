f91b94b9263f3abc6edda727cb28286cccfe2032
Juho Teperi <juho.teperi@iki.fi>
2015-09-27 19:06:01 +0300

    Release 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject deraen/lein-sass4clj "0.1.0"
+(defproject deraen/lein-sass4clj "0.1.1"
   :description "Leinigen task for Sass4clj"
   :url "https://github.com/Deraen/lein-sass4clj"
   :license {:name "Eclipse Public License"
